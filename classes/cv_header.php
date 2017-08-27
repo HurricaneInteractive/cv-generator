63c35fa3f8971d877e90fd4673eecbd4c126a1dc
HurricaneInteractive <>
2017-08-27 16:56:09 +1000

    Added getEmail function

--- classes/cv_header.php
+++ classes/cv_header.php
@@ -7,64 +7,80 @@ class CV_Header
      *
      *   @var string
      */
-     private $name = 'default';
+    private $name = 'default';
      
      /**
       *   Email of user
       *
       *   @var string
       */
-     private $email;
+    private $email;
  
      /**
       *   Phone number of user
       *
       *   @var integer
       */
-     private $phone_number;
+    private $phone_number;
  
      /**
       *   Address of user
       *
       *   @var string
       */
-     private $address; 
+    private $address; 
  
      /**
       *   Date of birth of user
       *
       *   @var date
       */
-     private $date_of_birth;
+    private $date_of_birth;
  
      /**
       *   Personal website of user
       *
       *   @var string
       */
-     private $personal_website;
+    private $personal_website;
 
-     function __construct($header_info) {
-         if (isset($header_info) && !empty($header_info)) {
-            // var_dump($header_info);
-            if (array_key_exists('name', $header_info)) {
-                $this->name = $header_info['name'];
-            }
-            if (array_key_exists('email', $header_info)) {
-                $this->email = $header_info['email'];
-            }
-         }
-         return $this->getHeaderInformation();
-     }
+    function __construct($header_info) {
+        if (isset($header_info) && !empty($header_info)) {
+        // var_dump($header_info);
+        if (array_key_exists('name', $header_info)) {
+            $this->name = $header_info['name'];
+        }
+        if (array_key_exists('email', $header_info)) {
+            $this->email = $header_info['email'];
+        }
+        }
+        return $this->getHeaderInformation();
+    }
 
-     public function getHeaderInformation() {
-         return array(
+    /**
+     *  Gets the "Header information" for the Resume
+     *
+     */
+    public function getHeaderInformation() {
+        return array(
             'name' => $this->name,
             'email' => $this->email
-         );
-     }
+        );
+    }
 
-     public function getName() {
-         return $this->name;
-     }
+    /**
+     *  Gets the users name
+     *  @return string
+     */
+    public function getName() {
+        return $this->name;
+    }
+
+    /**
+     *  Gets the users email
+     *  @return string
+     */
+    public function getEmail() {
+        return $this->email;
+    }
 }
