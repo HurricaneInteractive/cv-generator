3f2ef4cc1e8b3e97cc069180f0c582696bb8216a
HurricaneInteractive <hurricane.interactive@gmail.com>
2017-09-17 17:17:36 +1000

    CV_Header Class - improved documentation

--- app/Libraries/cv_header.php
+++ app/Libraries/cv_header.php
@@ -1,57 +1,60 @@
 <?php
 
+/**
+*   CV 'Header' class:
+*   This class contains information regarding the users personal information for the resume.
+*   @todo Integrate with User system to auto-fill values
+**/
+
 namespace App\Libraries;
 
 class CV_Header
 {
-    /**
-     *   Name of user
-     *
-     *   @var string
-     */
+/**
+    *   Name of user
+    *   @var string
+    */
     private $name;
      
-     /**
-      *   Email of user
-      *
-      *   @var string
-      */
+    /**
+    *   Email of user
+    *   @var string
+    */
     private $email;
  
-     /**
-      *   Phone number of user
-      *
-      *   @var string
-      */
+    /**
+    *   Phone number of user
+    *   @var string
+    */
     private $phone_number;
  
-     /**
-      *   Address of user
-      *
-      *   @var string
-      */
+    /**
+    *   Address of user
+    *   @var string
+    */
     private $address; 
  
-     /**
-      *   Date of birth of user
-      *
-      *   @var date
-      */
+    /**
+    *   Date of birth of user
+    *   @todo implement DOB in form
+    *   @var date
+    */
     private $date_of_birth;
  
-     /**
-      *   Personal website of user
-      *
-      *   @var string
-      */
+    /**
+    *   Personal website of user
+    *   @var string
+    */
     private $personal_website;
 
+    /**
+    *   Initializes the class
+    *   Uses information passed to it by the CV_PDF class
+    *   @see CV_PDF
+    *   @return the information as an array
+    */
     function __construct($header_info) {
-        // var_dump($header_info);
-        //die();
-
         if (isset($header_info) && !empty($header_info)) {
-            // var_dump($header_info);
             if (array_key_exists('name', $header_info)) {
                 $this->setName($header_info['name']);
             }
@@ -73,7 +76,7 @@ class CV_Header
 
     /**
      *  Gets the "Header information" for the Resume
-     *
+     *  @return array
      */
     public function getHeaderInformation() {
         return array(
@@ -86,9 +89,10 @@ class CV_Header
     }
 
     /**
-     *  @param string $name
-     *  @return string
-     */
+    *   Sets the $name variable based on user input
+    *   @param string $name
+    *   @return string
+    */
     public function setName($name) {
         if ($name != '' && $name != NULL) {
             $this->name = $name;
@@ -97,77 +101,81 @@ class CV_Header
     }
 
     /**
-     *  @param string $email
-     *  @return string
-     */
+    *   Sets the $email variable based on user input
+    *  @param string $email
+    *  @return string
+    */
     public function setEmail($email) {
         $this->email = $email;
         return $this;
     }
 
     /**
-     *  @param string $phone_number
-     *  @return string
-     */
+    *   Sets the $phone_number variable based on user input
+    *  @param string $phone_number
+    *  @return string
+    */
     public function setPhoneNumber($phone_number) {
         $this->phone_number = $phone_number;
         return $this;
     }
 
     /**
-     *  @param string $address
-     *  @return string
-     */
+    *   Sets the $address variable based on user input
+    *  @param string $address
+    *  @return string
+    */
     public function setAddress($address) {
         $this->address = $address;
         return $this;
     }
 
     /**
-     *  @param string $website
-     *  @return string
-     */
+    *   Sets the $personal_website variable based on user input
+    *  @param string $website
+    *  @return string
+    */
     public function setPersonalWebsite($website) {
         $this->personal_website = $website;
         return $this;
     }
 
     /**
-     *  Gets the users name
-     *  @return string
-     */
+    *  Gets the users name
+    *  @return string
+    */
     public function getName() {
         return $this->name;
     }
 
     /**
-     *  Gets the users email
-     *  @return string
-     */
+    *  Gets the users email
+    *  @return string
+    */
     public function getEmail() {
         return $this->email;
     }
 
     /**
-     *  Gets the users phone number
-     *  @return string
-     */
+    *  Gets the users phone number
+    *  @return string
+    */
     public function getPhoneNumber() {
         return $this->phone_number;
     }
 
     /**
-     *  Gets the users address
-     *  @return string
-     */
+    *  Gets the users address
+    *  @return string
+    */
     public function getAddress() {
         return $this->address;
     }
 
     /**
-     *  Gets the users personal website
-     *  @return string
-     */
+    *  Gets the users personal website
+    *  @return string
+    */
     public function getPersonalWebsite() {
         return $this->personal_website;
     }
